d4444c760bf13fe51ba37f687dc5814331a6fbb0
rashmisoundar <rashmiiyengar255@gmail.com>
2024-08-14 15:34:30 -0700

    My first test case

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
 
   e2e: {
+    baseUrl: 'https://webdriveruniversity.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -5,15 +5,23 @@ describe("Test Contact us form via WebDriveruniversity", ()=>{
         it("Should be able to submit a successfull submission via contactus form",()=>{
             //cypress code
             
-            cy.visit("https://webdriveruniversity.com")
-            cy.get('#contact-us').click({force: true});
-
+            cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html");
+            //cy.get('#contact-us').invoke('removeAttr', '__').click({force: true});
+            cy.get('[name="first_name"]').type("adam");
+            cy.get('[name="last_name"]').type("bill")
+            cy.get('[name="email"]').type("test@gmail.com")
+            cy.get('textarea.feedback-input').type("comments added")
+            cy.get('[type="submit"]').click();
         });
  
         it("Should not be able to submit a successfull submission via contact us form as all fields are required",()=>{
             //cypress code
-             
-
+            cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html");
+            cy.get('[name="first_name"]').type("add");
+            cy.get('[name="last_name"]').type("bill")
+            
+            cy.get('textarea.feedback-input').type("comments added")
+            cy.get('[type="submit"]').click();
             
         });
 
