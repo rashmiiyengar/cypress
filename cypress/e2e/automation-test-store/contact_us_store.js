c277f68fe6e490df4b52b382167d590f67112808
rashmisoundar <rashmiiyengar255@gmail.com>
2024-08-16 16:25:54 -0700

    Chai assertions

--- cypress/e2e/automation-test-store/contact_us_store.js
+++ cypress/e2e/automation-test-store/contact_us_store.js
@@ -14,39 +14,39 @@ describe("Test Contact us form via Automation Test store", ()=>{
        cy.get('#ContactUsFrm_email').type("joe@gmail.com");
        cy.get('#ContactUsFrm_enquiry').type("do you provide additional discounts ");
        cy.get('button[title="Submit"]').click();
-       //cy.get('.maintext').should('contain.text', 'CONTACT US');
+       cy.get('.mb40 > :nth-child(3)').should('have.text',"Your enquiry has been successfully sent to the store owner!");
 
     });
 
     /// <reference types="Cypress" />
-    describe("Selector examples", () => {
-    it("Examples of Selectors via WebdriverUni Contact Us Page", () => {
-        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    // describe("Selector examples", () => {
+    // it("Examples of Selectors via WebdriverUni Contact Us Page", () => {
+    //     cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
 
-        //By tag name
-        cy.get("input")
+    //     //By tag name
+    //     cy.get("input");
 
-        //By attribute name and value
-        cy.get("input[name='first_name']")
+    //     //By attribute name and value
+    //     cy.get("input[name='first_name']");
 
-        //By id
-        cy.get("#contact_me")
+    //     //By id
+    //     cy.get("#contact_me");
 
-        //By class
-        cy.get(".feedback-input")
+    //     //By class
+    //     cy.get(".feedback-input");
 
-        //By multiple classes
-        cy.get("[class='navbar navbar-inverse navbar-fixed-top']")
+    //     //By multiple classes
+    //     cy.get("[class='navbar navbar-inverse navbar-fixed-top']");
 
-        //By two different attributes
-        cy.get("[name='email'][placeholder='Email Address']")
+    //     //By two different attributes
+    //     cy.get("[name='email'][placeholder='Email Address']");
 
-        //By xpath
-        cy.xpath("//input[@name='first_name']")
-    })
+    //     //By xpath
+    //     cy.xpath("//input[@name='first_name']");
+
+    // })
 })
 
   
 
 
-})
