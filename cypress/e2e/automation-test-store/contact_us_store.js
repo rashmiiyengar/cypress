6f247a40f3729dc5027ba3ad01db9482d450e916
rashmisoundar <rashmiiyengar255@gmail.com>
2024-08-19 20:02:34 -0700

    selectors

--- cypress/e2e/automation-test-store/contact_us_store.js
+++ cypress/e2e/automation-test-store/contact_us_store.js
@@ -5,13 +5,14 @@
 describe("Test Contact us form via Automation Test store", ()=>{
 
     it("Should be able to submit a successfull submission via contact us form",()=>{
-       cy.visit("https://automationteststore.com/");
+        cy.visit("https://automationteststore.com/");
         cy.get("a[href$='contact']").click();
 
-      //cy.xpath("//a[contains(@href,'contact')]").click();
-      //cy.xpath("//input[@id='ContactUsFrm_first_name']").type("Joe");
+       //cy.xpath("//a[contains(@href,'contact')]").click();
+       //cy.xpath("//input[@id='ContactUsFrm_first_name']").type("Joe");
        cy.get('#ContactUsFrm_first_name').type("Joe");
        cy.get('#ContactUsFrm_email').type("joe@gmail.com");
+       cy.get('#ContactUsFrm_email').should('have.attr','name','email')
        cy.get('#ContactUsFrm_enquiry').type("do you provide additional discounts ");
        cy.get('button[title="Submit"]').click();
        cy.get('.mb40 > :nth-child(3)').should('have.text',"Your enquiry has been successfully sent to the store owner!");
--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -13,20 +13,18 @@ describe("Test Contact us form via WebDriveruniversity", ()=>{
             cy.get('[type="submit"]').click();
         });
  
-
-        it("Should not be able to submit a successfull submission via contact us form as all fields are required",()=>{
+        it.only("Should not be able to submit a successfull submission via contact us form as all fields are required",()=>{
             
             cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html");
             cy.get('[name="first_name"]').type("add");
             cy.get('[name="last_name"]').type("bill");
-            
             cy.get('textarea.feedback-input').type("comments added");
             cy.get('[type="submit"]').click();
-            
-        });
+            cy.get('body').contains('Error: all fields are required');
+             });
 
-        // Only opens a testcase below only
-        it.only("Visit home page",()=>{
+        // Only opens a testcase below 
+        it("Visit home page",()=>{
             
             cy.visit("/");
             
