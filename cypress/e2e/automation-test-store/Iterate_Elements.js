6e4469bdd58de148ffde25aa2a96dee428981aa2
rashmisoundar <rashmiiyengar255@gmail.com>
2024-08-22 12:13:33 -0700

    Alias and invoke

--- cypress/e2e/automation-test-store/Iterate_Elements.js
+++ cypress/e2e/automation-test-store/Iterate_Elements.js
@@ -3,7 +3,7 @@
 
 describe('Iterate over elements', () => {
 
-    it.only('Log information of ll haircare products', () => {
+    it('Log information of ll haircare products', () => {
         cy.visit("https://automationteststore.com/index.php?rt=content/contact");
         cy.get("a[href*='/category&path=']").contains("Hair Care").click();
 
@@ -12,15 +12,14 @@ describe('Iterate over elements', () => {
         });
     });
 
-    it.only('Add specific product to cart', () => {
+    it('Add specific product to cart', () => {
         cy.visit("https://automationteststore.com/index.php?rt=content/contact");
         cy.get("a[href*='/category&path=']").contains("Hair Care").click();
 
         cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list)=>{
             if($el.text().includes('Seaweed Conditioner')){
-                cy.log($el.text());
+                cy.wrap($el).click();
             }
-          
         });
 
     });
---
+++ cypress/e2e/automation-test-store/alias-invoke.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+/// <reference types="cypress-xpath" />
+
+describe('Alias and invoke', () => {
+
+    it('Validate a specific haircare product', () => {
+        cy.visit("https://automationteststore.com/index.php?rt=content/contact");
+        cy.get("a[href*='/category&path=']").contains("Hair Care").click();
+
+        cy.get('.fixed_wrapper .prdocutname').eq(0).invoke('text').as('productName');
+        cy.get('@productName').its('length').should('be.gt',5);
+        cy.get('@productName').should('include','Seaweed Conditioner');
+    });
+
+    it.only('Validate products on home page', () => {
+        cy.visit("https://automationteststore.com");
+
+        cy.get('.thumbnail').as('product');
+
+        cy.get('@product').should('have.length',16);
+        cy.get('@product').find('.productcart').invoke('attr','title').should('include','Add to Cart')
+
+        
+    });
+
+});
