b7718cb28757a0b5fc1140e4adb83c05d5a879a3
rashmiiyengar <rashmiiyengar255@gmail.com>
2024-08-23 17:35:21 -0700

    test

--- cypress/e2e/webdriver-uni/radiobutton.js
+++ cypress/e2e/webdriver-uni/radiobutton.js
@@ -23,7 +23,7 @@ describe("Radio buttons", ()=>{
 
         cy.get("[value='lettuce']").check();
         cy.get("[value='lettuce']").should('be.checked');
-        cy.get("[value='pumpkin']").should('not.be.checked');
+        cy.get("[value='pumpkin']").should('not.be.checked'); 
 
         cy.get("[value='cabbage']").should('be.disabled');
 
