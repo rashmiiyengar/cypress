e43e1550571ee357cff2ecc552d6f9752d7b3941
rashmiiyengar <rashmiiyengar255@gmail.com>
2024-10-04 14:58:14 -0700

    Implementing page object model

--- cypress/e2e/webdriver-uni/radiobutton.js
+++ cypress/e2e/webdriver-uni/radiobutton.js
@@ -7,7 +7,6 @@ describe("Radio buttons", ()=>{
         cy.visit("/");
         cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({force: true});
         cy.url().should('include','Dropdown-Checkboxes-RadioButtons');
-        
         cy.get('#radio-buttons').find("[type='radio']").first().check();
         cy.get('#radio-buttons').find("[type='radio']").eq(1).check();
     });
