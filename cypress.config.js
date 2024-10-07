c7a49ccf97e12b389d2ca0b5ba4b8a96407673cb
rashmiiyengar <rashmiiyengar255@gmail.com>
2024-10-07 13:30:06 -0700

    custum config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,41 @@
 const { defineConfig } = require("cypress");
+const fs = require('fs-extra');
+const path = require('path');
+
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve('cypress\\config', `${file}.json`);
+
+  if(!fs.existsSync(pathToConfigFile)) {
+    console.log("No custom config file found.");
+    return {};
+  }
+
+  return fs.readJson(pathToConfigFile);
+}
 
 module.exports = defineConfig({
   projectId: 'n11yn6',
 
   e2e: {
+
     defaultCommandTimeout:10000,
     pageLoadTimeout:120000,
-    
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      const file = config.env.configFile || ''
+
+      return getConfigurationByFile(file);
+
+    },
+
     env:{
       firstName:"Rash",
       webdriveruni_homepage: "https://webdriveruniversity.com",
     },
     baseUrl: 'https://webdriveruniversity.com',
     video: false, // Enable video recording
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
     //excludeSpecPattern:"cypress/e2e/other/*.js",
     chromeWebSecurity:false,
